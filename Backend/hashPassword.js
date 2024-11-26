6837b3c846564aac81bf82e9e442e364799f92df
praveenkumar262 <146455754+praveenkumar262@users.noreply.github.com>
2024-11-26 20:46:02 +0530

    Delete Backend directory

--- Backend/hashPassword.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'password123';  // The password you want to hash
-bcrypt.hash(password, 10, function(err, hashedPassword) {
-  if (err) throw err;
-  console.log(hashedPassword);  // This will print the hashed password
-});
